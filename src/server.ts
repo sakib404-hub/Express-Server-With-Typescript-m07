f41142487cd59647fa9a79321317e9835d02e954
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-05-16 22:32:45 +0600

    added the pool for the connection with the database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import express, { type Application, type Request, type Response } from "express";
+import {Pool} from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app : Application = express();
 const port = 5001;
 
-app.get('/', (req, res)=>{
+// this is the middleware to parse the incoming request body as JSON
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended : true}));
+
+
+const pool = new Pool({
+    connectionString : `postgresql://neondb_owner:${process.env.NEON_DB_PASS}@ep-weathered-frost-aqohlb74-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+})
+
+
+
+app.get('/', (req : Request, res : Response)=>{
    // res.send('Hello World!, this is the express server running with the typescript module system : ESNext');
 
    res.status(200).json(
@@ -26,6 +41,20 @@ app.get('/users', (req: Request, res: Response)=>{
             }
         ]
     });
+})
+
+app.post("/", async(req : Request, res : Response)=>{
+
+    const {name, organization, password} = req.body;
+
+    res.status(201).json({
+        message : 'User created successfully',
+        data : {
+            name,
+            organization
+        }
+    })
+
 })
 
 app.listen(port, ()=>{
